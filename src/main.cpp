ec1ad042ca6b1afed6eee8632f0db66fda18e1b8
COZMIKDX <tacocatdx@gmail.com>
2026-07-27 01:23:35 -0500

    Add detection beep frequency adjustment, PIR warmup period with button press to skip, on-board led indicates set up complete.

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,21 @@
 #include <M5Unified.h>
 #include <M5Cardputer.h>
 #include <HTTPClient.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 1
+#define LED_DATA_PIN 21
+
+CRGB leds[NUM_LEDS];
 
 const char * ssid = "@Hyatt_WiFi";
 const char * password = "";
-const char * webhook_url = "";
+const char * webhook_url = ""; // Discord webhook URL for sending messages. Leave empty if not using Discord.
 const char * ntp_server = "pool.ntp.org";
 const long cst_offset_sec = -6 * 3600; // Central Time Zone (UTC-6) daylight savings. 
+float detect_beep_frequency = 2000;
 
-const int pir_pin = 1;
+const int pir_pin = 2;
 bool muted = false;
 
 void sendDiscordMessage(char * message) { 
@@ -49,7 +56,7 @@ void sendNTFYMessage(String message) {
         http.begin("https://ntfy.sh/CZMK-PIR-Detector");
         http.addHeader("Content-Type", "text/plain");
 
-        int httpResponseCode = http.POST("[" + String(timestring) + "] " + message);
+        int httpResponseCode = http.POST("[" + String(timestring) + "] [" + String(M5Cardputer.Power.getBatteryLevel()) + "%]" + message);
 
         if (httpResponseCode > 0) {
             Serial.printf("Response: %d\n", httpResponseCode);
@@ -75,6 +82,13 @@ void keyboard_input() {
             }
             muted = !muted;
         }
+
+        if (M5Cardputer.Keyboard.isKeyPressed('1')) {
+            detect_beep_frequency = detect_beep_frequency - 100;
+        }
+        else if (M5Cardputer.Keyboard.isKeyPressed('2')) {
+            detect_beep_frequency = detect_beep_frequency + 100;
+        }
     }
 }
 
@@ -84,23 +98,42 @@ void setup() {
     // M5.begin();
     M5.Lcd.setBrightness(0);
 
+    FastLED.addLeds<WS2812, LED_DATA_PIN, GRB>(leds, NUM_LEDS);
+
     Serial.begin(115200);
 
     Serial.println("Connecting to WiFi...");
     WiFi.begin(ssid, password);
 
+    // Program will be stuck here until connected to WiFi
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
 
-
+    // Don't really need to check if I keep the above loop... Might change this later.
     if (WiFi.status() == WL_CONNECTED) {
         Serial.println("\nConnected to WiFi!");
         setupTime();
         sendNTFYMessage("PIR Detector is now online!");
     }
-    pinMode(pir_pin, INPUT);
+    pinMode(pir_pin, INPUT_PULLDOWN); // HC-SR501 outputs HIGH when motion is detected.
+
+    sendNTFYMessage("PIR device warming for 1 minute...");
+    leds[0] = CRGB::Red; // Set LED to red to indicate warming up
+    FastLED.show();
+    int start_time = millis();
+    // Wait for 1 minute to allow the PIR sensor to stabilize
+    while (millis() - start_time < 60000) {
+        M5Cardputer.update();
+        if (M5Cardputer.BtnA.wasPressed()) {
+            // skip PIR warm up.
+            break;
+        }
+    }
+    sendNTFYMessage("PIR device is now ready!");
+    leds[0] = CRGB::Green; // Set LED to green to indicate ready
+    FastLED.show();
 
     M5.Speaker.tone(1000, 200); // Play a tone to indicate the device has started
 }
@@ -111,11 +144,12 @@ bool lastMotionState = LOW;
 void loop() {
     keyboard_input();
     if (digitalRead(pir_pin) == HIGH) {
+        Serial.println(M5Cardputer.Power.getBatteryLevel());
         if (lastMotionState == LOW && millis() - lastMotionTime > motionDebounceTime) { // Check for motion and debounce
             Serial.println("Motion detected!");
 
             if (!muted) {
-                M5.Speaker.tone(2000, 200); // Play a tone to indicate motion detected
+                M5.Speaker.tone(detect_beep_frequency, 200); // Play a tone to indicate motion detected
             }
 
             lastMotionTime = millis();
